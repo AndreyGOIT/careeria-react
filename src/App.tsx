312a10ff2f55ba4206fd112a404f75f6557debab
Andrey Erokhin <andreyerokhin@MacBook-Air-Andrey.local>
2025-10-24 14:22:10 +0300

    add navbar & routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import { useState } from "react";
 import "./App.css";
 import CustomerList from "./componets/CustomerList.tsx";
 import Message from "./Message.tsx";
+// Navigate component for routing
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
   // Example state for message component
@@ -13,16 +18,47 @@ const App = () => {
 
   return (
     <>
-      <h1 className="title-center">
-        React + Vite <span className="highlight">/ .NET Core API</span>
-      </h1>
+      <Router>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Navbar.Brand href="/" style={{ paddingLeft: 15 }}>
+            Northwind App
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/customers">Customers</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
 
-      {showMessage && <Message message={message} isPositive={isPositive} />}
-      <CustomerList
-        setMessage={setMessage}
-        setShowMessage={setShowMessage}
-        setIsPositive={setIsPositive}
-      />
+        {showMessage && <Message message={message} isPositive={isPositive} />}
+
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="home-page">
+                <h1 style={{ textAlign: "center" }}>Home Page</h1>
+                <h2 className="title-center">
+                  React + Vite{" "}
+                  <span className="highlight">/ .NET Core API</span>
+                </h2>
+              </div>
+            }
+          />
+          <Route
+            path="/customers"
+            element={
+              <CustomerList
+                setMessage={setMessage}
+                setShowMessage={setShowMessage}
+                setIsPositive={setIsPositive}
+              />
+            }
+          />
+        </Routes>
+      </Router>
     </>
   );
 };
